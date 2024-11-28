ecb251a7da4690f800cae3a4a5a36e5341afea70
nasywaadt <nasywaadt19.com>
2024-11-28 08:53:52 +0700

    version 1

---
+++ tugas.py
@@ -0,0 +1,48 @@
+data_panen = {
+    'lokasi1' : {
+        'nama_lokasi': 'Kebun A',
+        'hasil_panen': {
+            'padi': 1200,
+            'jagung': 800,
+            'kedelai': 500,
+        }
+    },
+    'lokasi2' : {
+        'nama_lokasi': 'Kebun B',
+        'hasil_panen': {
+            'padi': 1500,
+            'jagung': 900,
+            'kedelai': 450,
+       }
+   },
+   'lokasi3' : {
+       'nama_lokasi': 'Kebun C',
+       'hasil_panen': {
+            'padi': 1100,
+            'jagung': 750,
+            'kedelai': 600,
+       }
+   } 
+}
+
+#Menampilkan seluruh data lokasi dan hasil panen
+print (data_panen)
+
+#Menampilkan jumlah hasil panen jagung dari lokasi2
+print(data_panen['lokasi2']['hasil_panen']['jagung'])
+
+#Menampilakan lokasi dari lokasi3
+print(data_panen['lokasi3']['nama_lokasi'])
+
+#Memasukkan jumlah hasil panen padi dan kedelai setiap lokasi ke dalam variabel yang berbeda
+for i, j in data_panen.items():
+    padi = j['hasil_panen']['padi']
+    kedelai = j['hasil_panen']['kedelai']
+    nama_lokasi = j['nama_lokasi']
+
+    #Membuat percabangan jika jumlah hasil panen padi lebih dari 1300 atau kedelai lebih dari 800 di suatu lokasi, maka lokasi tersebut memerlukan perhatian khusus. Jika tidak, maka lokasi tersebut dalam kondisi baik.
+    if padi > 1300 or kedelai > 800:
+        print(f"Lokasi {nama_lokasi} memerlukan perhatian khusus")
+    else:
+        print(f"Lokasi {nama_lokasi} dalam kondisi baik")
+
